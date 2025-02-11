635558010ef71c5ba6e9f39164d9a14fa8b414fa
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-02-10 23:23:16 -0600

    menuicon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,33 @@
+import { MenuIcon } from "lucide-react"
 import NewDocumenButton from "./NewDocumenButton"  
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"  
 
 function Sidebar() {
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
+
+        <Sheet>
+        <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40}/>
+        </SheetTrigger>
+        <SheetContent side='left'>
+            <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                <div>
+                    {/*options*/}
+                </div>
+            </SheetHeader>
+        </SheetContent>
+        </Sheet>
+
 
         <div className="hidden md:inline">
             <NewDocumenButton />
